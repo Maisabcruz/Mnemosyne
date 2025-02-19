86493259ccad59babb7e3e6a59ae4981afbebe69
FIC <fic@info.local>
2025-02-18 21:54:34 -0300

    Interações com o site

--- sandbox/repeticoes.js
+++ sandbox/repeticoes.js
@@ -1,30 +1,51 @@
 // Nível básico***
 // Atividade 1 usando o FOR
-// for (let contador = 1; contador <= 10; contador++) {
 
-//     console.log("Contagem: ", contador);
+function exercicioRep01 () {
 
-// }
+    for (let contador = 1; contador <= 10; contador++) {
+
+        console.log("Contagem: ", contador);
+    
+    }
+
+    let divDelogs = document.querySelector(".logs");
+
+    let paragrafo = document.createElement("p");
+
+    paragrafo.innerHTML = "Exercicio 01 de repetição executado.";
+
+    divDelogs.appendChild(paragrafo);
+
+}
+
 //////////////////////////////////////////////
+
 // Atividade 1 usando o WHILE
-// let contador = 1;
-// while (contador <= 5) {
+function exercicioRep02 () {
+    let contador = 1;
+    while (contador <= 5) {
 
-//     console.log("Contagem: ", contador);
-//     contador++
+    console.log("Contagem: ", contador);
+    contador++
 
-// }
+    }
+}
+
 //////////////////////////////////////////////
+
 // Atividade 1 usando o DO
 // let mensagem = "";
 // do {
 
-//     mensagem = prompt("Digite um texto");
+// mensagem = prompt("Digite um texto");
 
 // } while (mensagem != "sair");
 
 // console.log("Saiu!");
+
 ///////////////////////////////////////////////
+
 // Atividade 2 usando o FOR
 // let num = prompt("insira um número: ");
 
@@ -37,7 +58,9 @@
 //     console.log(mensagem);
 
 // }
+
 ///////////////////////////////////////////////
+
 // Atividade 3 usando N, While e For
 // let num = parseInt(prompt("Insira um número N: "));
 
@@ -50,19 +73,62 @@
 //      console.log(mensagem);
 
 // }
+
 //////////////////////////////////////////////////
+
 // Nível Intermediário***
 // Atividade 1
+// for (let contador = 1; contador <= 50; contador++){
+
+//     if (contador % 2 == 0)
+
+//         console.log(contador);
+
+// } 
+
+///////////////////////////////////////////////////////////
 
 // Atividade 2
+// let numeroAleatorio = Math.floor(Math.random() * 100);
+
+// let chute = null;
+
+// while (chute != numeroAleatorio) {
+
+//     chute = parseInt(prompt("Chute um número:"));
+
+//     if (chute != numeroAleatorio) {
+
+//         if (chute > numeroAleatorio) {
+    
+//             alert("Chutou alto.");
+    
+//         } else {
+    
+//             alert("Chutou baixo.");
+    
+//         }
+
+//     }
+
+// }
+
+//////////////////////////////////////////////////
 
 // Atividade 3
 
+
+
 //////////////////////////////////////////////////
+
 // Nível Avançaco***
 // Atividade 1
 
+
 // Atividade 2
+
+
+// alert("Você acertou!");
 
 // Atividade 3
 
