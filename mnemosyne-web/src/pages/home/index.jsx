932cbbc8f74a469902e12fb73e39c1bd70d7054f
FIC <fic@info.local>
2025-02-25 21:47:44 -0300

    Tela de add memoria PRONTA!

--- mnemosyne-web/src/pages/home/index.jsx
+++ mnemosyne-web/src/pages/home/index.jsx
@@ -59,7 +59,7 @@ const Home = () => {
 
             <footer className="app-footer">
 
-                <p>Mnemosyne - Ariel Paixão dos Santos</p>
+                <p>Mnemosyne - @maisabcruz</p>
 
             </footer>
         </>
--- mnemosyne-web/src/services/memory.js
+++ mnemosyne-web/src/services/memory.js
@@ -1,8 +1,8 @@
-const API_UTL = "http://localhost:3000";
+const API_URL = "http://localhost:3000";
 
 const getMemories = async () => {
 
-    const response = await fetch(`${API_UTL}/memories`);'' //Busquei os dados.
+    const response = await fetch(`${API_URL}/memories`);'' //Busquei os dados.
 
     const memories = await response.json(); //converte a requisição para json.
 
@@ -10,4 +10,35 @@ const getMemories = async () => {
 
 }
 
-export default { getMemories };
+const createMemory = async (memory) => {
+    const images = memory.images; // Obtém a lista de imagens da memória
+    const base64Images = []; // Array para armazenar as imagens convertidas para Base64
+
+    for (let i = 0; i < images.length; i++) { // Percorre todas as imagens
+        const base64 = await imageToBase64(images[i]); // Converte a imagem para Base64
+        base64Images.push(base64); // Adiciona a imagem convertida ao array
+    }
+
+    memory.images = base64Images; // Substitui a lista de imagens pelos valores convertidos para Base64
+
+    const response = await fetch(`${API_URL}/memories`, {
+        method: "POST", // Define o método HTTP como POST
+        headers: {
+            "Content-Type": "application/json" // Define o cabeçalho da requisição como JSON
+        },
+        body: JSON.stringify(memory) // Converte o objeto da memória para JSON e envia no corpo da requisição
+    });
+
+    return response.json(); // Retorna a resposta da API convertida para JSON
+}
+
+function imageToBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader(); // Cria um leitor de arquivos
+        reader.readAsDataURL(file); // Lê o arquivo como um Data URL (Base64)
+        reader.onload = () => resolve(reader.result); // Quando a leitura for concluída, resolve a promessa com o resultado Base64
+        reader.onerror = error => reject(error); // Se houver erro, rejeita a promessa
+    });
+}
+
+export default { getMemories, createMemory};
